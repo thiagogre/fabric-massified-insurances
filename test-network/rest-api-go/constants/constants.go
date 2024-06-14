a04c4ed7fde9c67589576b2fdf42688d0ff6f978
thiagogre <thiagogre.dev@gmail.com>
2024-06-14 12:30:57 -0300

    add printing messages logger and shell colors

--- test-network/rest-api-go/constants/constants.go
+++ test-network/rest-api-go/constants/constants.go
@@ -2,4 +2,10 @@ package constants
 
 const (
 	TestNetworkPath = "../"
+
+	RedColorOuput      = "\033[31m"
+	GreenColorOuput    = "\033[32m"
+	YellowColorOutput  = "\033[33m"
+	BlueColorOutput    = "\033[34m"
+	DefaultColorOutput = "\033[0m"
 )
---
+++ test-network/rest-api-go/pkg/logger/logger.go
@@ -0,0 +1,61 @@
+package logger
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path"
+	"rest-api-go/constants"
+	"runtime"
+)
+
+var (
+	InfoLogger    *log.Logger
+	WarnLogger    *log.Logger
+	SuccessLogger *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func Init() {
+	InfoLogger = log.New(os.Stdout, constants.BlueColorOutput+"[INFO] ", log.Ldate|log.Ltime)
+	WarnLogger = log.New(os.Stdout, constants.YellowColorOutput+"[WARN] ", log.Ldate|log.Ltime)
+	SuccessLogger = log.New(os.Stderr, constants.GreenColorOuput+"[SUCCESS] ", log.Ldate|log.Ltime)
+	ErrorLogger = log.New(os.Stderr, constants.RedColorOuput+"[ERROR] ", log.Ldate|log.Ltime)
+}
+
+func logMessage(logger *log.Logger, format string, args ...interface{}) {
+	functionInvocationLevelsUp := 3
+	_, file, line, _ := runtime.Caller(functionInvocationLevelsUp)
+	filename := path.Base(file)
+	logger.Printf("[%s:%d] "+format+constants.DefaultColorOutput, append([]interface{}{filename, line}, args...)...)
+}
+
+func Info(message interface{}) {
+	Log(InfoLogger, message)
+}
+
+func Warn(message interface{}) {
+	Log(WarnLogger, message)
+}
+
+func Success(message interface{}) {
+	Log(SuccessLogger, message)
+}
+
+func Error(message interface{}) {
+	Log(ErrorLogger, message)
+}
+
+func Log(logger *log.Logger, message interface{}) {
+	switch msg := message.(type) {
+	case string:
+		logMessage(logger, msg)
+	default:
+		jsonString, err := json.Marshal(msg)
+		if err != nil {
+			logMessage(logger, "%v", msg)
+		}
+
+		logMessage(logger, "%s", jsonString)
+	}
+}
