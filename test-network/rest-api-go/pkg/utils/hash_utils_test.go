57a7561ed1007b142c50a257bdb52cbbb1c16651
thiagogre <thiagogre.dev@gmail.com>
2024-09-06 10:56:01 -0300

    add static values

--- test-network/rest-api-go/pkg/utils/hash_utils_test.go
+++ test-network/rest-api-go/pkg/utils/hash_utils_test.go
@@ -4,31 +4,25 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"golang.org/x/crypto/bcrypt"
 )
 
-// hashPassword helper function to hash a password for testing purposes
-func hashPassword(password string) string {
-	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(hashedPassword)
-}
+const (
+	PASSWORD        = "TestUser"
+	HASHED_PASSWORD = "$2a$10$Zlq4NHbbVu60EvboKwNY6eUUyyFy2fNldqfQCn7Cs5bnvnN10CjK6"
+)
 
 // TestCheckPasswordHash tests for different scenarios using shared setup
 func TestCheckPasswordHash(t *testing.T) {
-	// Setup common test data
-	password := "valid_password"
-	hashedPassword := hashPassword(password)
-
 	t.Run("CorrectPassword", func(t *testing.T) {
-		require.True(t, CheckPasswordHash(password, hashedPassword))
+		require.True(t, CheckPasswordHash(PASSWORD, HASHED_PASSWORD))
 	})
 
 	t.Run("WrongPassword", func(t *testing.T) {
 		wrongPassword := "wrongpassword"
-		require.False(t, CheckPasswordHash(wrongPassword, hashedPassword))
+		require.False(t, CheckPasswordHash(wrongPassword, HASHED_PASSWORD))
 	})
 
 	t.Run("EmptyPassword", func(t *testing.T) {
-		require.False(t, CheckPasswordHash("", hashedPassword))
+		require.False(t, CheckPasswordHash("", HASHED_PASSWORD))
 	})
 }
