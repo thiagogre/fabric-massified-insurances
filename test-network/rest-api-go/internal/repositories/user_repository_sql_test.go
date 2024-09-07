9a2c22cd0c557e22e9203487279e555cc3a7a25e
thiagogre <thiagogre.dev@gmail.com>
2024-09-06 22:09:46 -0300

    add user repository unit tests

---
+++ test-network/rest-api-go/internal/repositories/user_repository_sql_test.go
@@ -0,0 +1,29 @@
+package repositories
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/thiagogre/fabric-massified-insurances/test-network/rest-api-go/constants"
+	"github.com/thiagogre/fabric-massified-insurances/test-network/rest-api-go/tests"
+)
+
+func TestAuthenticateUser(t *testing.T) {
+	testDB, err := tests.Setup()
+	require.NoError(t, err)
+	defer testDB.Close()
+
+	repo := SQLUserRepository{DB: testDB}
+
+	t.Run("ValidUser", func(t *testing.T) {
+		user, err := repo.GetUserById(constants.TestUsername)
+		require.NoError(t, err)
+		require.Equal(t, constants.TestUsername, user.Id)
+	})
+
+	t.Run("NonexistentUser", func(t *testing.T) {
+		user, err := repo.GetUserById("nonexistent")
+		require.Error(t, err)
+		require.Nil(t, user)
+	})
+}
