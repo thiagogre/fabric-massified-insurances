7a078a379a2de89dde308ea24e7088c5320ce436
thiagogre <thiagogre.dev@gmail.com>
2024-09-15 19:58:35 -0300

    ensure fresh test results

--- test-network/rest-api-go/run_tests.sh
+++ test-network/rest-api-go/run_tests.sh
@@ -5,6 +5,8 @@ ARGS=$1
 set -e
 
 echo "Running unit tests..."
+go clean -testcache
+go test -count=1
 go test $ARGS $(go list ./... | grep -v '/tests/integration') || {
     echo "Tests failed"
     exit 1
