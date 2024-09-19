d775e7e4f1bb04b0d194ce41c4aa4a1e67fbbe3a
thiagogre <thiagogre.dev@gmail.com>
2024-09-19 14:40:09 -0300

    add btn type

--- test-network/frontend-react/components/button/Button.tsx
+++ test-network/frontend-react/components/button/Button.tsx
@@ -2,12 +2,27 @@ import React from "react";
 
 import type { ButtonProps } from "./types";
 
-const Button = ({ children, onClick, disabled = false }: ButtonProps) => {
-	return (
+const Button = ({
+	children,
+	onClick,
+	disabled = false,
+	type = "primary",
+}: ButtonProps) => {
+	return type === "primary" ? (
 		<button
+			type="button"
 			disabled={disabled}
 			onClick={onClick}
-			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md"
+		>
+			{children}
+		</button>
+	) : (
+		<button
+			type="button"
+			disabled={disabled}
+			onClick={onClick}
+			className="px-4 py-2 bg-gray-200 hover:bg-gray-400 text-black rounded-md"
 		>
 			{children}
 		</button>
--- test-network/frontend-react/components/button/types.ts
+++ test-network/frontend-react/components/button/types.ts
@@ -2,6 +2,7 @@ type ButtonProps = {
 	children: React.ReactNode;
 	onClick: () => void;
 	disabled?: boolean;
+	type?: "primary" | "secondary";
 };
 
 export type { ButtonProps };
