e633896424d2dd5d0f45fbddcb443d8a9268c5db
thiagogre <thiagogre.dev@gmail.com>
2024-09-20 18:15:06 -0300

    implement get/update asset repository

--- test-network/rest-api-go/internal/adapters/claim_repository.go
+++ test-network/rest-api-go/internal/adapters/claim_repository.go
@@ -1,12 +1,19 @@
 package adapters
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"mime/multipart"
+	"net/http"
 	"os"
 	"path/filepath"
 
 	"io"
 
+	"github.com/thiagogre/fabric-massified-insurances/test-network/rest-api-go/constants"
+	"github.com/thiagogre/fabric-massified-insurances/test-network/rest-api-go/internal/domain"
+	"github.com/thiagogre/fabric-massified-insurances/test-network/rest-api-go/internal/dto"
 	"github.com/thiagogre/fabric-massified-insurances/test-network/rest-api-go/pkg/logger"
 )
 
@@ -41,6 +48,72 @@ func (r *ClaimRepository) SaveFile(file *multipart.FileHeader, uploadDir, filena
 	if err != nil {
 		logger.Error("Error saving file: " + err.Error())
 		return err
+	}
+
+	return nil
+}
+
+func (h *ClaimRepository) GetAsset(username string) (*domain.Asset, error) {
+	URL := fmt.Sprintf(
+		"http://localhost%s/smartcontract/query?channelid=%s&chaincodeid=%s&function=GetAssetsByRichQuery&args=%s",
+		constants.ServerAddr,
+		constants.ChannelID,
+		constants.ChaincodeID,
+		fmt.Sprintf(`{"selector":{"Insured":"%s"}}`,
+			username))
+
+	resp, err := http.Get(URL)
+	if err != nil {
+		logger.Error("Failed to call API: " + err.Error())
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		logger.Error(fmt.Sprintf("API request failed with status: %d", resp.StatusCode))
+		return nil, fmt.Errorf("API request failed with status: %d", resp.StatusCode)
+	}
+
+	var response dto.SuccessResponse[dto.DocsResponse[domain.Asset]]
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		logger.Error("Failed to decode response: " + err.Error())
+		return nil, fmt.Errorf("failed to decode API response: %w", err)
+	}
+
+	if len(response.Data.Docs) == 0 {
+		logger.Error("No assets found in response")
+		return nil, fmt.Errorf("no assets found for user: %s", username)
+	}
+
+	return &response.Data.Docs[0], nil
+}
+
+func (h *ClaimRepository) UpdateAsset(asset *domain.Asset, uploadDir string) error {
+	URL := fmt.Sprintf("http://localhost%s/smartcontract/invoke", constants.ServerAddr)
+	body := dto.InvokeRequest{
+		ChannelID:   constants.ChannelID,
+		ChaincodeID: constants.ChaincodeID,
+		Function:    "UpdateAsset",
+		Args:        []string{asset.ID, asset.Insured, fmt.Sprintf("%d", asset.CoverageDuration), fmt.Sprintf("%d", asset.CoverageValue), fmt.Sprintf("%d", asset.CoverageType), asset.Partner, fmt.Sprintf("%d", asset.Premium), "Pending", uploadDir},
+	}
+
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		logger.Error("Failed to marshal JSON: " + err.Error())
+		return err
+	}
+
+	resp, err := http.Post(URL, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		logger.Error("Failed to call API: " + err.Error())
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		logger.Error(fmt.Sprintf("API request failed with status: %d", resp.StatusCode))
+		return fmt.Errorf("API request failed with status: %d", resp.StatusCode)
 	}
 
 	return nil
